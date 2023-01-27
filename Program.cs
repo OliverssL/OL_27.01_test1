99ad9678de4106951ed82b4f0c48c5ee22e6f120
OliverssL <oliverslaizans@gmail.com>
2023-01-27 08:55:09 +0200

    1 augsupielade

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace OL_27._01_test1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
